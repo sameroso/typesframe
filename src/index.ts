b508f8fd0da38f7e810f78178484cac0d184cbd7
Samer Kayali <samerkkr@gmail.com>
2021-10-25 22:06:04 -0300

    feat: render content on screen

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-import { User } from "./models/User";
+import { UserForm } from "./Views/UserForm";
 
-const user = new User({ name: "new user", age: 10 });
-
-user.on("change", () => console.log("User was changed"));
-user.set({ name: "Samer", id: 2 });
-
-user.save()
+const userForm = new UserForm(document.getElementById("root"));
+userForm.render()
