3195d848e09e086088a2b6a88608b290eea758c3
Samer Kayali <samerkkr@gmail.com>
2021-10-26 15:29:58 -0300

    feat: added strict mode

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { UserForm } from "./Views/UserForm";
 import { User } from "./models/User";
 
 const user = User.buildUser({ name: "NAME", age: 20 });
+const root = document.getElementById("root");
 
-const userForm = new UserForm(document.getElementById("root"), user);
-userForm.render();
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error("Root element not found");
+}
