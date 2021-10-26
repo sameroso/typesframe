7ca9db30bd4ed0209cc475662c77957826a0aef3
Samer Kayali <samerkkr@gmail.com>
2021-10-26 20:37:41 -0300

    feat: adding generic class View

---
+++ src/Views/View.ts
@@ -0,0 +1,35 @@
+import { Model } from "../models/Model";
+
+export abstract class View<T extends Model<K>,K> {
+  constructor(public parent: Element, public model: T) {
+    this.bindModel();
+  }
+
+  bindModel() {
+    this.model.on("change", () => {
+      this.render();
+    });
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(":");
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
+  abstract template(): string;
+  abstract eventsMap(): { [key: string]: () => void };
+
+  render(): void {
+    this.parent.innerHTML = "";
+    const templateElement = document.createElement("template");
+    templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
+    this.parent.append(templateElement.content);
+  }
+}
