bfce375af0d594ccd01ae659f4bd204025df091d
Samer Kayali <samerkkr@gmail.com>
2021-10-27 09:09:20 -0300

    feat: implmented collection view

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,34 @@
-import { UserEdit } from "./Views/UserEdit";
-import { User } from "./models/User";
+import { User, UserProps } from "./models/User";
+import { UserList } from "./Views/UserList";
+import { Collection } from "./models/Collection";
 
-const user = User.buildUser({ name: "Samer", age: 20 });
-const root = document.getElementById("root");
+// const user = User.buildUser({ name: "Samer", age: 20 });
+const users = new Collection(
+  "http://localhost:3000/users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-} else {
-  throw new Error("Root element not found");
-}
+users.on("change", () => {
+  const root = document.getElementById("root");
+
+  if (root) {
+    const userList = new UserList(root, users);
+    userList.render();
+  } else {
+    throw new Error("Root element not found");
+  }
+});
+
+
+users.fetch()
+
+// const root = document.getElementById("root");
+
+// if (root) {
+//   const userEdit = new UserEdit(root, user);
+//   userEdit.render();
+// } else {
+//   throw new Error("Root element not found");
+// }
