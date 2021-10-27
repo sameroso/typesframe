c68775ab8abaed40a2080fe83bed9a30d4db01bf
Samer Kayali <samerkkr@gmail.com>
2021-10-26 21:19:40 -0300

    feat: added hability to  nest views

--- src/Views/View.ts
+++ src/Views/View.ts
@@ -1,6 +1,7 @@
 import { Model } from "../models/Model";
 
-export abstract class View<T extends Model<K>,K> {
+export abstract class View<T extends Model<K>, K> {
+  regions: { [key: string]: Element } = {};
   constructor(public parent: Element, public model: T) {
     this.bindModel();
   }
@@ -22,14 +23,38 @@ export abstract class View<T extends Model<K>,K> {
     }
   }
 
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap();
+    for (let key in regionsMap) {
+      const selector = regionsMap[key];
+      const element = fragment.querySelector(selector);
+      if (element) {
+        this.regions[key] = element;
+      }
+    }
+  }
+
   abstract template(): string;
-  abstract eventsMap(): { [key: string]: () => void };
+
+  regionsMap(): { [key: string]: string } {
+    return {};
+  }
+
+  eventsMap(): { [key: string]: () => void } {
+    return {};
+  }
+
+  onRender(): void {}
 
   render(): void {
     this.parent.innerHTML = "";
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
     this.bindEvents(templateElement.content);
+    this.mapRegions(templateElement.content);
+
+    this.onRender();
+
     this.parent.append(templateElement.content);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { UserForm } from "./Views/UserForm";
+import { UserEdit } from "./Views/UserEdit";
 import { User } from "./models/User";
 
 const user = User.buildUser({ name: "Samer", age: 20 });
 const root = document.getElementById("root");
 
 if (root) {
-  const userForm = new UserForm(root, user);
-  userForm.render();
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
 } else {
   throw new Error("Root element not found");
 }
